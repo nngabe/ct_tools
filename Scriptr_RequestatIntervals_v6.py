948e8a544d94603c7012bbcd7992551e0be31d6e
Rhys-L <53022924+Rhys-L@users.noreply.github.com>
2020-05-12 14:03:01 -0400

    CT code change for page selection dropdown
    added updated selectors to chose a page, scratch for selecting kor vax

--- Scriptr_RequestatIntervals_v6.py
+++ Scriptr_RequestatIntervals_v6.py
@@ -76,9 +76,22 @@ ScopeDropdown_Button = browser.find_element_by_css_selector('#select_scope_chzn 
 sleep(1)
 ByList_Button = browser.find_element_by_css_selector("#select_scope_chzn_o_1").click()
 
-PagesDropdown_Button = browser.find_element_by_css_selector('#sel4GW_chzn > a:nth-child(1) > div:nth-child(2)').click()
+
+PagesDropdown_Button = browser.find_element_by_css_selector('#sel9W3_chzn').click()
 sleep(1)
-NewNeutral_Button = browser.find_element_by_css_selector('#sel4GW_chzn_o_19').click()
+
+##struggling to select dropdown choice below
+mySelectElement = browser.find_element_by_css_selector('#sel9W3')
+dropDownMenu = Select(mySelectElement)
+WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[3]/div/div[3]/div/div/form/section[1]/div[2]/select/optgroup[1]")))
+dropDownMenu.select_by_visible_text('kor_vax')
+
+#css selector for list of pages optgroup.select-option:nth-child(1) 
+#css selector for KOR vax optgroup.select-option:nth-child(1) > option:nth-child(13)
+
+
+browser.find_element_by_xpath('/html/body/div[3]/div/div[3]/div/div/form/section[1]/div[2]/select/optgroup[1]/option[13]')
+#xpath kor vax /html/body/div[3]/div/div[3]/div/div/form/section[1]/div[2]/select/optgroup[1]/option[13]
 
 
 
