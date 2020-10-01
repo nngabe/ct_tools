b672b446eed2d57bbdc741d2aac7dc68060d5f66
nngabe <nickgabriel8@gmail.com>
2020-10-01 14:19:34 -0400

    made keyword search nicer

--- search_keyword.py
+++ search_keyword.py
@@ -18,7 +18,11 @@ from bs4 import BeautifulSoup
 
 from ct_utils import fb_login, get_driver
 
-def search_links(links_file, rootdir):
+def search_keywords(kw_file, rootdir):
+    """
+    Search keywords in crowdtangle and send csv to email with top posts containg the keyword. 
+    Defaults search is for facebook posts with most interactions over the last 24 hours.
+    """
     rtic = lambda n: randint(1,n) # lag to simulate manual data collection
 
     ### get FB/CrowdTangle username and password from INI file
@@ -37,20 +41,20 @@ def search_links(links_file, rootdir):
     driver.get('https://apps.crowdtangle.com/search/home')
     sleep(4+rtic(4))
 
-    ### build directory structure to write data
-    outdir = rootdir + 'out_search'
-    if not (os.path.exists(outdir)):
-        os.mkdir(outdir)
-    dt_string = datetime.datetime.now().strftime("%d-%m-%Y_%H_%M_%S")
-    write_dir = outdir + '/' + dt_string
-    os.mkdir(write_dir)
-    cp(links_file,write_dir)
+    ### build directory structure to write data, not needed for now
+    #outdir = rootdir + 'out_search'
+    #if not (os.path.exists(outdir)):
+    #    os.mkdir(outdir)
+    #dt_string = datetime.datetime.now().strftime("%d-%m-%Y_%H_%M_%S")
+    #write_dir = outdir + '/' + dt_string
+    #os.mkdir(write_dir)
+    #cp(kw_file,write_dir)
     
-    links_df = pd.read_csv(links_file,index_col=0)
-    indices = list(links_df.index)
-    links = list(links_df.links)
+    keywords_df = pd.read_csv(kw_file,index_col=0)
+    indices = list(keywords_df.index)
+    keywords = list(keywords_df.keyword)
 
-    for idx,link in zip(indices,links):    
+    for idx,keyword in zip(indices,keywords):    
         
         ### clear search box
         try:
@@ -62,20 +66,18 @@ def search_links(links_file, rootdir):
         ### entering a new search 
         search_box = driver.find_element_by_xpath('//input[starts-with(@class,"searchBar")]')
         search_box.click()
-        search_box.send_keys(link)
+        search_box.send_keys(keyword)
         search_box.send_keys(Keys.ENTER)
-        #return driver
         sleep(6+rtic(4))
 
         download_button = driver.find_element_by_xpath('//div[starts-with(@class,"searchOptions__optionBtnContainer")]')
-        download_button.click()    
+        download_button.click() # somehow knows to click download csv button    
     
-    return download_button
-    #driver.close()
+    driver.close()
             
 if __name__ == '__main__':
 
     rootdir = './'
-    links_file = 'keywords.csv'
-    download_button = search_links(links_file, rootdir)
+    kw_file = 'keywords.csv'
+    search_keywords(kw_file, rootdir)
 
